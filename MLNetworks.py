b6eb6d91ec54e9a02115cf13900005471253c132
HosseinMehriB <70452050+HosseinMehriB@users.noreply.github.com>
2025-04-16 13:14:57 -0600

    Update MLNetworks.py

--- MLNetworks.py
+++ MLNetworks.py
@@ -7,6 +7,27 @@ Created on Thu May 25 10:41:31 2023
 @author: HOSSEINMEHRI
 
 This file contains the machine learning methods used in traffic prediction task.
+
+Citation:
+If you find this code useful in your research, please consider citing our paper:
+
+H. Mehri, H. Mehrpouyan, and H. Chen, "RACH Traffic Prediction in Massive Machine Type Communications," IEEE Transactions on Machine Learning in Communications and Networking, vol. 3, pp. 315–331, 2025.
+
+[IEEE Xplore Link](https://ieeexplore.ieee.org/document/10891603) | DOI: 10.1109/TMLCN.2025.3542760
+
+```
+@ARTICLE{10891603,
+  author={Mehri, Hossein and Mehrpouyan, Hani and Chen, Hao},
+  journal={IEEE Transactions on Machine Learning in Communications and Networking}, 
+  title={RACH Traffic Prediction in Massive Machine Type Communications}, 
+  year={2025},
+  volume={3},
+  number={},
+  pages={315-331},
+  keywords={Prediction algorithms;Long short term memory;Machine learning;Accuracy;3GPP;Predictive models;Complexity theory;Base stations;Traffic control;Telecommunication traffic;Massive machine-type communications;internet of things;machine learning;traffic prediction;smart cities},
+  doi={10.1109/TMLCN.2025.3542760}}
+```
+
 """
 
 import torch
